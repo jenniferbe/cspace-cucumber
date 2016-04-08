63f00209c8d236dcabe6dbf3d5c9366030b223a4
cesarvh <cesarv.h@berkeley.edu>
2016-04-07 21:13:54 -0700

    Added mappings for more Record types

--- src/test/java/org/collectionspace/qa/records/LocationMovementInventory.java
+++ src/test/java/org/collectionspace/qa/records/LocationMovementInventory.java
@@ -22,7 +22,7 @@ public class LocationMovementInventory extends Record {
       xpathMap.put("Normal Location",          "//*[@id=\"primaryTab\"]/div/div[3]/div[1]/div[2]/div[1]/div[2]/div/div[2]/input[2]"); //
       xpathMap.put("Current Location",         "//*[@id=\"primaryTab\"]/div/div[3]/div[1]/div[2]/div[2]/div/div[2]/table/tbody/tr/td[1]/input[2]"); //
       xpathMap.put("Contact",                  "//*[@id=\"primaryTab\"]/div/div[3]/div[2]/div[2]/div[1]/div[2]/div/div[2]/input[2]"); //
-      xpathMap.put("Second Contact",           "//*[@id=\"fluid-id-5jxc7z9x-1204\"]/ul/li/input[3]"); /
+      xpathMap.put("Second Contact",           "//*[@id=\"fluid-id-5jxc7z9x-1204\"]/ul/li/input[3]"); 
 
     }
 }
